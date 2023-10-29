77d2f42cb29ea19213b51d4a9230d03ce817a396
stareginio <sophiecodes02@gmail.com>
2023-10-29 13:01:17 +0800

    Create CS26115_SevenSegmentDisplay_Reginio.java

---
+++ src/CS26115_SevenSegmentDisplay_Reginio.java
@@ -0,0 +1,78 @@
+
+import java.awt.*;
+import java.awt.geom.*;
+
+public class CS26115_SevenSegmentDisplay_Reginio {
+    
+    Graphics2D g2;
+    
+    public CS26115_SevenSegmentDisplay_Reginio(Graphics2D g2) {
+        this.g2 = g2;
+    }
+    
+    public void drawDigit() {
+        // == Variables ==========
+        int[] triangleX;
+        int[] triangleY;
+        int rectX, rectY, rectWidth, rectHeight;
+        Area colon, upperLeftSegment, upperRightSegment, lowerLeftSegment,
+                lowerRightSegment, topSegment, middleSegment, lowerSegment;
+        
+        // == Create shapes for the segment ==========
+        rectX = 30;
+        rectY = 30;
+        rectWidth = 8;
+        rectHeight = 30;
+        
+        // -- Create the base shape ----------
+        Rectangle2D.Double rect = new Rectangle2D.Double(
+                rectX, rectY, rectWidth, rectHeight
+        );
+        upperLeftSegment = new Area(rect);
+        
+        // -- Subtract the upper left corner ----------
+        triangleX = new int[] { rectX, rectX, (rectX + rectWidth/2) };
+        triangleY = new int[] { (rectX + rectWidth/2), rectY, rectY };
+        Polygon triangle = new Polygon(triangleX, triangleY, 3);
+        
+        upperLeftSegment.subtract(new Area(triangle));
+        
+        // -- Subtract the upper right corner ----------
+        triangle = new Polygon(triangleX, triangleY, 3);
+        
+        AffineTransform at = new AffineTransform();
+        at.rotate(Math.toRadians(90), (rectX + rectWidth/2), ((rectY+rectHeight)/2));
+        at.translate(rectWidth/2, -rectWidth/2);
+        
+        Shape transformedShape = at.createTransformedShape(triangle);
+        upperLeftSegment.subtract(new Area(transformedShape));
+        
+        // -- Subtract the bottom right corner ----------
+        triangle = new Polygon(triangleX, triangleY, 3);
+        
+        at = new AffineTransform();
+        at.rotate(Math.toRadians(180), (rectX + rectWidth/2), ((rectY+rectHeight)/2));
+        at.translate(0, -rectHeight);
+
+        transformedShape = at.createTransformedShape(triangle);
+        upperLeftSegment.subtract(new Area(transformedShape));
+        
+        // -- Subtract the bottom left corner ----------
+        triangle = new Polygon(triangleX, triangleY, 3);
+        
+        at = new AffineTransform();
+        at.rotate(Math.toRadians(270), (rectX + rectWidth/2), ((rectY+rectHeight)/2));
+        at.translate(-(rectHeight - rectWidth/2), -rectWidth/2);
+
+        transformedShape = at.createTransformedShape(triangle);
+        upperLeftSegment.subtract(new Area(transformedShape));
+        
+        // -- Create the segment ----------        
+        g2.setColor(Color.black);
+        g2.fill(upperLeftSegment);
+        
+        // for debugging
+//        g2.setColor(Color.red);
+//        g2.fill(new Area(transformedShape));
+    }
+}
