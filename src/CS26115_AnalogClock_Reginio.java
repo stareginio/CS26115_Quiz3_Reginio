42142e4971aa88d667270519fcec79023c4219bb
stareginio <sophiecodes02@gmail.com>
2023-10-30 15:45:58 +0800

    Update CS26115_AnalogClock_Reginio.java
    added all the hour and minute marks

--- src/CS26115_AnalogClock_Reginio.java
+++ src/CS26115_AnalogClock_Reginio.java
@@ -14,22 +14,26 @@ public class CS26115_AnalogClock_Reginio {
             int digitWidth, int dist) {
         // == Variables ==========
         int outerRadius, innerRadius, middleRadius,
-                circleCenterX, circleCenterY,
-                hourMarkWidth, hourMarkHeight, hourMarkDist;
-        Area analogClockBase, analogClockMiddle, hourMark;
+                circleCenterX, circleCenterY, timeMarkX, timeMarkY,
+                hourMarkWidth, hourMarkHeight, minuteMarkWidth, minuteMarkHeight,
+                timeMarkDist;
+        Area analogClockBase, analogClockMiddle, timeMarks;
+        Rectangle2D.Double hourMark, minuteMark;
+        Shape transformedShape;
+        AffineTransform at;
         
         // == Create the base shape ==========
         outerRadius = panelWidth - panelWidth/4;
         circleCenterX = panelWidth/2 - outerRadius/2;
         circleCenterY = panelHeight/2 - outerRadius/2 + digitWidth + dist;
         
-        // black circle
+        // -- Black circle ----------
         Ellipse2D.Double circle = new Ellipse2D.Double(
                 circleCenterX, circleCenterY, outerRadius, outerRadius
         );
         analogClockBase = new Area(circle);
         
-        // white circle
+        // -- White circle ----------
         innerRadius = outerRadius - dist*4;
         circle = new Ellipse2D.Double(
                 circleCenterX + dist*2, circleCenterY + dist*2,
@@ -37,7 +41,7 @@ public class CS26115_AnalogClock_Reginio {
         );
         analogClockBase.subtract(new Area(circle));
         
-        // small red circle
+        // -- Small red circle ----------
         middleRadius = 10;
         circle = new Ellipse2D.Double(
                 panelWidth/2 - middleRadius/2,
@@ -46,18 +50,67 @@ public class CS26115_AnalogClock_Reginio {
         );
         analogClockMiddle = new Area(circle);
         
-        // == Create the hour marks ==========
-        hourMarkWidth = 7;
+        // == Create the time marks ==========
+        hourMarkWidth = 5;
         hourMarkHeight = 16;
-        hourMarkDist = 20;
         
-        Rectangle2D.Double rect = new Rectangle2D.Double(
-                panelWidth/2 - hourMarkWidth/2, circleCenterY + hourMarkDist,
+        minuteMarkWidth = 3;
+        minuteMarkHeight = 10;
+        
+        timeMarkDist = 20;
+        timeMarkX = panelWidth/2;
+        timeMarkY = panelHeight/2 + digitWidth + dist;
+        
+        // -- Hour mark ----------
+        hourMark = new Rectangle2D.Double(
+                panelWidth/2 - hourMarkWidth/2, circleCenterY + timeMarkDist,
                 hourMarkWidth, hourMarkHeight
         );
-        hourMark = new Area(rect);
+        timeMarks = new Area(hourMark);
         
-        analogClockBase.add(hourMark);
+        // -- Minute mark ----------
+        // 1st mark
+        minuteMark = new Rectangle2D.Double(
+                panelWidth/2 - hourMarkWidth/2, circleCenterY + timeMarkDist,
+                minuteMarkWidth, minuteMarkHeight
+        );
+        
+        at = new AffineTransform();
+        at.rotate(Math.toRadians(6), timeMarkX, timeMarkY);
+        transformedShape = at.createTransformedShape(minuteMark);
+        timeMarks.add(new Area(transformedShape));
+        
+        // 2nd mark
+        at.rotate(Math.toRadians(6), timeMarkX, timeMarkY);
+        transformedShape = at.createTransformedShape(minuteMark);
+        timeMarks.add(new Area(transformedShape));
+        
+        // 3rd mark
+        at.rotate(Math.toRadians(6), timeMarkX, timeMarkY);
+        transformedShape = at.createTransformedShape(minuteMark);
+        timeMarks.add(new Area(transformedShape));
+        
+        // 4th mark
+        at.rotate(Math.toRadians(6), timeMarkX, timeMarkY);
+        transformedShape = at.createTransformedShape(minuteMark);
+        timeMarks.add(new Area(transformedShape));
+        
+//        // -- 12 o' clock ----------
+        analogClockBase.add(timeMarks);
+        
+        // -- 1 to 11 o' clock ----------
+        at = new AffineTransform();
+        for (int i=1; i < 12; i++) {
+            at.rotate(Math.toRadians(30), timeMarkX, timeMarkY);
+            transformedShape = at.createTransformedShape(timeMarks);
+            timeMarks.add(new Area(transformedShape));
+        }
+        
+        analogClockBase.add(timeMarks);
+
+        // for debugging
+//        g2.setColor(Color.red);
+//        g2.fill(timeMarks);
         
         // == Create the analog clock without the hands ==========
         g2.setColor(Color.black);
