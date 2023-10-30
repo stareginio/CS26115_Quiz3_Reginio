4d3660d61ae171239be2808bce6e3a2e9f886daf
stareginio <sophiecodes02@gmail.com>
2023-10-30 23:16:53 +0800

    Create CS26115_IntlClock_Reginio.java

---
+++ src/CS26115_IntlClock_Reginio.java
@@ -0,0 +1,13 @@
+
+import javax.swing.*;
+
+public class CS26115_IntlClock_Reginio extends JPanel {
+    
+    private String clockName;
+    
+    public CS26115_IntlClock_Reginio(String clockName) {
+        this.clockName = clockName;
+    }
+    
+    
+}
