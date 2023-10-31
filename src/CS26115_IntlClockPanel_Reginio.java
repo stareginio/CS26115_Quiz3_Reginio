d56c5f8b4e6ded0b6c5d426c34326d8e9a103073
stareginio <sophiecodes02@gmail.com>
2023-10-31 16:33:24 +0800

    Update CS26115_IntlClockPanel_Reginio.java
    fixed international clocks

--- src/CS26115_IntlClockPanel_Reginio.java
+++ src/CS26115_IntlClockPanel_Reginio.java
@@ -1,14 +1,17 @@
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.*;
 
 public class CS26115_IntlClockPanel_Reginio extends JPanel {
     
-    private final String clockName;
+    private final String name;
+    private final int[] time;
     
     // Constructor
-    public CS26115_IntlClockPanel_Reginio(String clockName) {
-        this.clockName = clockName;
+    public CS26115_IntlClockPanel_Reginio(String name, int[] time) {
+        this.name = name;
+        this.time = time;
     }
     
     @Override
@@ -16,18 +19,14 @@ public class CS26115_IntlClockPanel_Reginio extends JPanel {
         Graphics2D g2 = (Graphics2D) g;
         super.paintComponent(g2);
         
-        // == Variables ==========
-        int startingPoint, digitWidth, colonLength, dist;
-        
-        // == Time ==========
-        
-        
         // == Analog clock ==========
         CS26115_AnalogClock_Reginio ac = new CS26115_AnalogClock_Reginio(g2);
         
         ac.paintBase(getWidth(), getHeight(), 0, 0);
-//        ac.paintHands(getWidth(), hr, min, sec);
+        ac.paintHands(getWidth(), time[0], time[1], time[2]);
         ac.paintRedCircle(getWidth(), getHeight(), 0, 0);
         
+        setAlignmentY(Component.CENTER_ALIGNMENT);
+        setAlignmentX(Component.CENTER_ALIGNMENT);
     }
 }
